6e7cac2f9388bce4f09f880d16ad0e97136c7a11
Lukas Bixen <lrb2_@outlook.com>
2022-12-04 02:32:47 -0600

    Added help system

---
+++ lrbot/commands/help.py
@@ -0,0 +1,46 @@
+import discord
+import os
+import lrbot.response
+
+helpFolder = os.path.join(
+    os.sep,
+    os.getcwd(),
+    'help'
+)
+
+async def run(message: discord.Message) -> None:
+    args = message.content.lower().split()
+
+    source = 'help'
+
+    if len(args) > 1:
+        source = args[1]
+    
+    helpPath = helpFolder + os.sep + source
+    
+    if not os.path.exists(helpPath):
+        await lrbot.response.reactToMessage(message, 'fail')
+        return
+    
+    await lrbot.response.reactToMessage(message, 'success')
+
+    helpFile = open(helpPath)
+    helpContents = helpFile.read()
+    helpFile.close()
+
+    helpTitle = 'lrbot Help'
+    if source != 'help':
+        helpTitle = helpTitle + ': $' + source
+
+    helpEmbed = discord.Embed(
+        title = helpTitle,
+        type = 'article',
+        description = helpContents
+    )
+
+    await lrbot.response.sendResponse(
+        message.channel,
+        reference = message,
+        embed = helpEmbed
+    )
+    return
